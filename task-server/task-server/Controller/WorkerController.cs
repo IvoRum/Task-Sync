37dfd71707f4c78b73edb630262d89e66f8e883f
IvoRum <ivailo.rumenov@outlook.com>
2024-03-11 00:13:49 +0200

    Added input handaling for password useng composite pattern.

--- task-server/task-server/Controller/WorkerController.cs
+++ task-server/task-server/Controller/WorkerController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using task_server.Model;
 using task_server.Repository;
+using task_server.Service.validation;
 
 namespace task_server.Controller
 {
@@ -22,7 +23,15 @@ namespace task_server.Controller
             WorkerRepository workerRepository=new WorkerRepository();
             return workerRepository.GetWorkersById(workerId);
         }
-
+                
+        [HttpGet("/worker/lgin/{password}/{workerId}")]
+        public ActionResult<Worker> Login(string password,int workerId)
+        {
+            WorkerRepository workerRepository=new WorkerRepository();
+            Ivalidator validator = new PasswordValidator(password);
+            validator.validate();
+            return workerRepository.GetWorkersById(workerId);
+        }
 
     }
 }
---
+++ task-server/task-server/Service/validation/StringValidator.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+using task_server.Model.input;
+
+namespace task_server.Service.validation;
+
+public class StringValidator : Ivalidator
+{
+    private string stringToValidat;
+    private Regex regex;
+
+    public StringValidator(string stringToValidat, string pattern)
+    {
+        this.stringToValidat = stringToValidat;
+        Regex regex = new Regex(pattern);
+    }
+
+    public string StringToValidat
+    {
+        get => stringToValidat;
+        set => stringToValidat = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public void validate()
+    {
+
+
+
+        if (regex.IsMatch(stringToValidat))
+        {
+            Console.WriteLine("Input string contains only English letters.");
+        }
+        else
+        {
+            Console.WriteLine("Input string contains non-English letters.");
+            throw new NotImplementedException();
+        }
+    }
+}
