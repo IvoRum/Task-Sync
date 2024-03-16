5ad99603fdbfb5713e526dec7f50beb008350831
IvoRum <ivailo.rumenov@outlook.com>
2024-03-16 09:59:44 +0200

    Created index

--- task-sql/Fuctions.sql
+++ task-sql/Fuctions.sql
@@ -1,21 +1,3 @@
-CREATE FUNCTION WorkersOnProject(@workerName as VARCHAR)
-    RETURNS @Result TABLE
-                    (
-                        tickate_name varchar,
-                        work_time    bigint,
-                        due          DATE,
-                        worker_name  varchar
-                    )
-AS
-BEGIN
-    INSERT INTO @Result
-    select *
-    from WrokerOnProjectWhitTasks wp
-    where wp.worker_name = @workerName
-    RETURN;
-end;
-
-
 
 CREATE FUNCTION WorkersOnProject(@workerName as VARCHAR(100))
     RETURNS table
